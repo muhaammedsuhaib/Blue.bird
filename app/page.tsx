9f4ca502948d2a5df4f03551b672f45da1fb1c88
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-10-05 17:32:38 +0530

    fix : promblem

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
       </div>
 
       {/* Brand Logo Section */}
-      <div className="mt-12">
+      <div className="flex items-center justify-center mt-12">
         <Image
           src="/bglogo.png"
           alt="Blue Bird Logo"
